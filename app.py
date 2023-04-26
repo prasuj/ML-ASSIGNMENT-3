fe43ca6d43ae99d6f1510e6852e62a70fdb16410
prasuj <113792523+prasuj@users.noreply.github.com>
2023-04-27 00:16:00 +0530

    updated

--- app.py
+++ app.py
@@ -1,90 +1,69 @@
-import seaborn as sns
-import numpy as np # linear algebra
-import matplotlib.pyplot as plt   # Graphics
-from matplotlib import colors
-import seaborn                    # Graphics
-import geopandas as gpd           # Spatial data manipulation
-import pandas as pd               # Tabular data manipulation
-import xarray                     # Surface data manipulation
-from pysal.explore import esda    # Exploratory spatial analytics
-from pysal.lib import weights     # Spatial weights
-import contextily                 # Background tiles
-import geoplot as gplt
-import geoplot.crs as gcrs
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import contextily
+import seaborn
+from libpysal import weights
 import streamlit as st
 
-# save filepath to variable for easier access
-od_filepath = 'https://raw.githubusercontent.com/prasuj/ML-ASSIGNMENT-3/main/CDC_Injury_Center_Drug_Overdose_Deaths.csv?token=GHSAT0AAAAAACBNGTT7WHBNQEU7LTNDON4IZCI4W7Q'
-# read the data and store data in dataframe
-# choosing to save as pandas, not geopandas
-# adding encoding to prevent a 'UnicodeDecodeError'
-oddb = pd.read_csv(od_filepath, encoding = "ISO-8859-1")
+# Load the shapefile containing the state boundaries
 
+contiguous_usa = pd.read_csv('output.csv')
 
-# finding data with state shapes and joining it
-contiguous_usa = gpd.read_file(gplt.datasets.get_path('contiguous_usa'))
-result = contiguous_usa.set_index('state').join(oddb.set_index('State'))
-# set up figure and a single axis
-g, ax = plt.subplots(1, figsize=(9, 9))
+# Load the drug overdose data
+overdoses = pd.read_csv('CDC_Injury_Center_Drug_Overdose_Deaths.csv', encoding="ISO-8859-1")
 
-# build choropleth
-result.plot(
-    column='2019 Age-adjusted Rate (per 100,000 population)',
-    cmap='Blues',
-    scheme='quantiles',
-    k=5,
-    edgecolor='0.8',
-    linewidth=0.8,
-    alpha=0.75,
-    legend=True,
-    legend_kwds=dict(loc=2),
-    ax=ax
-)
+# Join the overdose data with the state boundaries based on state name
+result = contiguous_usa.set_index('state').join(overdoses.set_index('State'))
 
-# add basemap
-contextily.add_basemap(
-    ax,
-    crs=result.crs,
-    source=contextily.providers.CartoDB.VoyagerNoLabels
-)
+# set up the Streamlit app
+st.title('US Drug Overdose Rates')
+st.write('Visualization of US drug overdose rates by state')
 
-# remove axis
-ax.set_axis_off();
+# create a slider for k
+k = st.slider('Select k for KNN weights', min_value=1, max_value=20, value=8, step=1)
 
 # generate W from the GeoDataFrame
-w = weights.distance.KNN.from_dataframe(result, k=8)
+w = weights.distance.KNN.from_dataframe(result, k=k)
 # row-standardization
 w.transform = 'R'
 
-
 result['w_2019 Age-adjusted Rate (per 100,000 population)'] = weights.spatial_lag.lag_spatial(w, result['2019 Age-adjusted Rate (per 100,000 population)'])
-result['2019 Age-adjusted Rate (per 100,000 population)_std'] = ( result['2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean() )
-result['w_2019 Age-adjusted Rate (per 100,000 population)_std'] = ( result['w_2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean() )
 
-# create a slider to adjust k value
-k = st.slider('Select k value:', 2, 10, 8, 1)
-
-# update the W matrix based on the slider value
-w = weights.distance.KNN.from_dataframe(result, k=k)
-w.transform = 'R'
+result['2019 Age-adjusted Rate (per 100,000 population)_std'] = (result['2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean())
+result['w_2019 Age-adjusted Rate (per 100,000 population)_std'] = (result['w_2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean())
 
 # setup the figure and axis
-f, ax = plt.subplots(1, figsize=(6,  6))
+fig, ax = plt.subplots(1, figsize=(6, 6))
 # plot values
-seaborn.regplot(
-    x='2019 Age-adjusted Rate (per 100,000 population)_std',
-    y='w_2019 Age-adjusted Rate (per 100,000 population)_std',
-    data=result,
-    ax=ax,
-    line_kws={'color': 'red'},
-    scatter_kws={'alpha': 0.5}
-)
-# set x and y axis labels
-ax.set_xlabel('2019 Age-adjusted Rate (per 100,000 population)')
-ax.set_ylabel('Spatial Lag of 2019 Age-adjusted Rate (per 100,000 population)')
+seaborn.regplot(x='2019 Age-adjusted Rate (per 100,000 population)_std', y='w_2019 Age-adjusted Rate (per 100,000 population)_std', data=result, ci=None)
+plt.title('Spatial Autocorrelation for US Drug Overdose Rates')
+plt.xlabel('Age-adjusted Rate (per 100,000 population)')
+plt.ylabel('Spatial Lag of Age-adjusted Rate (per 100,000 population)')
 
-# set plot title
-ax.set_title(f'Spatial Lag plot using {k} neighbors')
+# set up figure and a single axis
+fig, ax = plt.subplots(1, figsize=(9, 9))
 
-# remove borders
-sns.despine(ax=ax, left=True, bottom=True)
+# build choropleth
+result.plot(column='2019 Age-adjusted Rate (per 100,000 population)',
+            cmap='Blues',
+            scheme='quantiles',
+            k=5,
+            edgecolor='0.8',
+            linewidth=0.8,
+            alpha=0.75,
+            legend=True,
+            legend_kwds=dict(loc=2),
+            ax=ax)
+
+# add basemap
+contextily.add_basemap(ax,
+                        crs=result.crs,
+                        source=contextily.providers.CartoDB.VoyagerNoLabels)
+
+# remove axis
+ax.set_axis_off()
+
+# show the plots in Streamlit
+st.pyplot(fig)
+st.pyplot(seaborn) # Show seaborn plot
