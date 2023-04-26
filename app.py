bec081e0554283c1ddc1189114e4cadff249e26a
prasuj <113792523+prasuj@users.noreply.github.com>
2023-04-26 09:48:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-import streamlit as st
+import seaborn as sns
 import numpy as np # linear algebra
 import matplotlib.pyplot as plt   # Graphics
 from matplotlib import colors
@@ -12,37 +11,28 @@ from pysal.lib import weights     # Spatial weights
 import contextily                 # Background tiles
 import geoplot as gplt
 import geoplot.crs as gcrs
+import streamlit as st
 
-
-# set page title
-st.set_page_config(page_title="Drug Overdose Deaths in the USA")
-
-# set up sidebar
-st.sidebar.title("Settings")
-k = st.sidebar.slider("K", min_value=1, max_value=20, value=8)
-scheme = st.sidebar.selectbox("Choropleth Scheme", ["quantiles", "equal_interval", "fisher_jenks"])
-basemap = st.sidebar.selectbox("Basemap", ["CartoDB Positron", "CartoDB Dark_Matter", "Stamen Terrain", "Stamen Toner", "OpenStreetMap Mapnik", "OpenTopoMap"])
-normalize = st.sidebar.checkbox("Normalize values")
-
-# load data
+# save filepath to variable for easier access
 od_filepath = 'https://raw.githubusercontent.com/prasuj/ML-ASSIGNMENT-3/main/CDC_Injury_Center_Drug_Overdose_Deaths.csv?token=GHSAT0AAAAAACBNGTT7WHBNQEU7LTNDON4IZCI4W7Q'
-oddb = pd.read_csv(od_filepath, encoding="ISO-8859-1")
+# read the data and store data in dataframe
+# choosing to save as pandas, not geopandas
+# adding encoding to prevent a 'UnicodeDecodeError'
+oddb = pd.read_csv(od_filepath, encoding = "ISO-8859-1")
 
+
+# finding data with state shapes and joining it
 contiguous_usa = gpd.read_file(gplt.datasets.get_path('contiguous_usa'))
 result = contiguous_usa.set_index('state').join(oddb.set_index('State'))
-
 # set up figure and a single axis
 g, ax = plt.subplots(1, figsize=(9, 9))
 
 # build choropleth
-column = '2019 Age-adjusted Rate (per 100,000 population)'
-if normalize:
-    column += '_std'
 result.plot(
-    column=column,
+    column='2019 Age-adjusted Rate (per 100,000 population)',
     cmap='Blues',
-    scheme=scheme,
-    k=k,
+    scheme='quantiles',
+    k=5,
     edgecolor='0.8',
     linewidth=0.8,
     alpha=0.75,
@@ -55,40 +45,46 @@ result.plot(
 contextily.add_basemap(
     ax,
     crs=result.crs,
-    source=contextily.providers[basemap]
+    source=contextily.providers.CartoDB.VoyagerNoLabels
 )
 
 # remove axis
 ax.set_axis_off();
 
 # generate W from the GeoDataFrame
-w = weights.distance.KNN.from_dataframe(result, k=k)
+w = weights.distance.KNN.from_dataframe(result, k=8)
 # row-standardization
 w.transform = 'R'
 
-result[f'w_{column}'] = weights.spatial_lag.lag_spatial(w, result[column])
 
-if normalize:
-    result[column] = (result[column] - result[column].mean()) / result[column].std()
-    result[f'w_{column}'] = (result[f'w_{column}'] - result[f'w_{column}'].mean()) / result[f'w_{column}'].std()
+result['w_2019 Age-adjusted Rate (per 100,000 population)'] = weights.spatial_lag.lag_spatial(w, result['2019 Age-adjusted Rate (per 100,000 population)'])
+result['2019 Age-adjusted Rate (per 100,000 population)_std'] = ( result['2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean() )
+result['w_2019 Age-adjusted Rate (per 100,000 population)_std'] = ( result['w_2019 Age-adjusted Rate (per 100,000 population)'] - result['2019 Age-adjusted Rate (per 100,000 population)'].mean() )
+
+# create a slider to adjust k value
+k = st.slider('Select k value:', 2, 10, 8, 1)
+
+# update the W matrix based on the slider value
+w = weights.distance.KNN.from_dataframe(result, k=k)
+w.transform = 'R'
 
 # setup the figure and axis
 f, ax = plt.subplots(1, figsize=(6,  6))
 # plot values
 seaborn.regplot(
-    x=column, y=f'w_{column}', data=result, ci=None
+    x='2019 Age-adjusted Rate (per 100,000 population)_std',
+    y='w_2019 Age-adjusted Rate (per 100,000 population)_std',
+    data=result,
+    ax=ax,
+    line_kws={'color': 'red'},
+    scatter_kws={'alpha': 0.5}
 )
+# set x and y axis labels
+ax.set_xlabel('2019 Age-adjusted Rate (per 100,000 population)')
+ax.set_ylabel('Spatial Lag of 2019 Age-adjusted Rate (per 100,000 population)')
 
-# set plot title and axis labels
-ax.set_title('Spatial Autocorrelation')
-ax.set_xlabel(column)
-ax.set_ylabel(f'Spatial Lag of {column}')
+# set plot title
+ax.set_title(f'Spatial Lag plot using {k} neighbors')
 
-# calculate Moran's I
-moran = esda.Moran(result[column], w)
-
-# add Moran's I to the plot
-ax.annotate(f'Moran\'s I = {moran.I:.2f}\np-value = {moran.p_sim:.2f}', xy=(0.05, 0.85), xycoords='axes fraction')
-
-# show plot
-st.pyplot(f)
+# remove borders
+sns.despine(ax=ax, left=True, bottom=True)
